1d9693fb212a4c56e45a2cc8f36ec83387188f32
WilliamBZA <william.brander@gmail.com>
2016-05-12 19:40:57 +0200

    Finished saga and added start of sagas

---
+++ Samples/MsmqSamples/SagaInvasionCoordinator/Program.cs
@@ -0,0 +1,62 @@
+﻿using Messages;
+using Messages.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SagaInvasionCoordinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class InvasionSaga : IHandle<InvadeCountryCommand>,
+        IHandle<ManufactureTanksCommand>
+    {
+        public void Handle(InvadeCountryCommand message)
+        {
+            SetTimeout();
+            throw new NotImplementedException();
+        }
+
+        private void SetTimeout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ManufactureTanksCommand message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(TanksManufacturedEvent message)
+        {
+
+        }
+
+        public /*override*/ void TimeTriggered(DateTime triggerTime)
+        {
+
+            MarkAsAborted();
+            MarkAsCompleted();
+        }
+
+
+        // saga methods
+        private void MarkAsAborted()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MarkAsCompleted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
