900dc8030030f45f94927a8158b3b4f9e8853343
WilliamBZA <william.brander@gmail.com>
2016-05-16 08:08:50 +0200

    Added a delete all queues project

---
+++ Samples/MsmqSamples/DeleteQueues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queues = MessageQueue.GetPrivateQueuesByMachine(".");
+
+            foreach (var queue in queues)
+            {
+                MessageQueue.Delete(queue.Path);
+            }
+        }
+    }
+}
